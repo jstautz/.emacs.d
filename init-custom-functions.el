2c233d9a0dc3e2aaa91176d50fa55114daf90f4b
Jeff Stautz <jeff.stautz@gmail.com>
2013-08-29 20:31:29 -0700

    Added functions to rename/delete current buffer's file

--- init-custom-functions.el
+++ init-custom-functions.el
@@ -219,7 +219,7 @@ of windows in the frame simply by calling this command again."
 ;; requested item and then makes a new try to locate it.
 ;; by Jonas.Jarnestrom<at>ki.ericsson.se
 ;; Fri Mar 15 09:52:14 2002
-;; -----------------------------------------------------------------------------
+;;-----------------------------------------------------------------------------
 (defadvice find-tag (around refresh-etags activate)
   "Rerun etags and reload tags if tag not found and redo find-tag.
    If buffer is modified, ask about save before running etags."
@@ -246,6 +246,48 @@ of windows in the frame simply by calling this command again."
    center screen on cursor"
   (let ((current-prefix-arg '(4)))
   (call-interactively 'recenter-top-bottom)))
+
+
+;;-----------------------------------------------------------------------------
+;; Renaming and deleting files should be easier to do. 
+;; C-x C-r renames current file
+;; C-x C-k deletes current file (since C-x k kills the buffer)
+;;-----------------------------------------------------------------------------
+
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+
+(global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
+
+
+(defun delete-current-buffer-file ()
+  "Removes file connected to current buffer and kills buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (ido-kill-buffer)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename t)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
+
+(global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
 
 
 (provide 'init-custom-functions)
