863e3f9d907b36a3f11cb4fa3137c0fdb4864161
Jeff Stautz <jeff.stautz@gmail.com>
2013-08-29 20:29:05 -0700

    Additional smex toggle options C-x C-m

--- config/init-smex.el
+++ config/init-smex.el
@@ -7,14 +7,8 @@
         (global-set-key (kbd "M-x") 'smex)
         (global-set-key (kbd "M-X") 'smex-major-mode-commands)
         (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-        (add-hook 'org-mode-hook
-                  (lambda()
-                    (define-key org-mode-map
-                      (kbd "\C-c\C-m") 'smex)))
-        (add-hook 'org-mode-hook
-                  (lambda()
-                    (define-key org-mode-map
-                      (kbd "\C-x\C-m") 'smex)))
+        (global-set-key (kbd "C-c C-m") 'smex)
+        (global-set-key (kbd "C-x C-m") 'smex)
         (smex))
     (error (execute-extended-command))))
 (global-set-key (kbd "M-x") 'jcs:smex-init)
