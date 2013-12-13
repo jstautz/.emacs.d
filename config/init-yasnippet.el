12c467b9d71627cf7c415bc2ed36ffa2c13647fe
Jeff Stautz <jeff.stautz@gmail.com>
2013-12-12 23:43:51 -0800

    Minor changes to yasnippet config

--- config/init-yasnippet.el
+++ config/init-yasnippet.el
@@ -1,13 +1,12 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 (yas-load-directory (concat dotemacs-dir "/snippets/"))
-(yas-load-directory (concat dotemacs-dir "/el-get/feature-mode/snippets"))
+;;(yas-load-directory (concat dotemacs-dir "/el-get/feature-mode/snippets"))
 
 ;; Override markdown-mode's tab
-(add-hook 'markdown-mode-hook 
-          #'(lambda () 
-              (local-set-key [tab] 'yas-expand)))
+;; (add-hook 'markdown-mode-hook 
+;;           #'(lambda () 
+;;               (local-set-key [tab] 'yas-expand)))
 
 ;; disable yas in org-mode agenda -- may be causing crashes
 (setq yas-dont-activate "*Org Agenda*")
-
