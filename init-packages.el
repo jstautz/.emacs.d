5a72eb8fd1c434d7448f4abb7d5227f7e0ad1e13
Jeff Stautz <jeff.stautz@gmail.com>
2013-12-12 23:41:43 -0800

    Updated package lists Cut down non-essentials, added a few others. Also simplified sync section.

--- init-packages.el
+++ init-packages.el
@@ -9,7 +9,6 @@
 (require 'package)
 (dolist (source '( ("gnu" . "http://elpa.gnu.org/packages/")
                    ("elpa" . "http://tromey.com/elpa/")
-                   ("technomancy" . "http://repo.technomancy.us/emacs/")
                    ("marmalade" . "http://marmalade-repo.org/packages/")
                    ("melpa" . "http://melpa.milkbox.net/packages/")))
   (add-to-list 'package-archives source t))
@@ -30,80 +29,32 @@
       el-get-user-package-directory (concat dotemacs-dir "config"))
 
 
-;; el-get Recipes 
+;; My custom el-get recipes 
 (setq el-get-sources
       '(
+        (:name ace-jump-mode
+               :website "https://github.com/winterTTr/ace-jump-mode/wiki"
+               :description "A quick cursor location minor mode for emacs"
+               :type github
+               :pkgname "winterTTr/ace-jump-mode"
+               :features ace-jump-mode ace-jump-mode-pop-mark)
 
-        (:name android
-               :description "Included with Android SDK - connect Emacs to emulator"
-               :type http
-               :url "https://raw.github.com/evnm/emacs/master/vendor/android.el"
-               :compile ("android.el")
-               :features android)
-        
-;; TODO -- fix this build script so it performs a sed replace of ECUKES_EL and ECUKES_EMACS vars on install.
-        (:name ecukes
-               :description "Cucumber integration testing framework for Emacs"
-               :type git
-               :url "https://github.com/rejeep/ecukes"
-               :build ("ln -s ~/.emacs.d/el-get/ecukes/ecukes ~/Dropbox/bin/ecukes"))
-
-        (:name edb
-               :description "Emacs database mode"
-               :type git
-               :url "https://github.com/emacsmirror/edb"
-               :build `(,(concat "./configure --prefix=" el-get-dir "edb/ --infodir=" info-dir
-                                 " --with-sitelisp=" el-get-dir "edb/lisp/")
-                        ,(concat "make EMACS=" emacs-bin)
-                        "make install")
-               :load-path "lisp"
-               :features database)
-
-        (:name elip
-               :description "Emacs Learning mode (flashcards)"
-               :type http
-               :url "https://raw.github.com/emacsmirror/elip/master/source/elip.el"
-               :depends edb
-               :features elip)
-
-        (:name espuds
-               :description "Ecukes step definitions"
-               :type git
-               :url "https://github.com/rejeep/espuds")
-
-        (:name htmlize
-               :description "Convert buffer text and decorations to HTML."
-               :type git
-               :url "http://fly.srk.fer.hr/~hniksic/emacs/htmlize.git")
+        (:name commander
+               :type github
+               :description "Emacs command line parser"
+               :pkgname "rejeep/commander.el"
+               :depends (f s dash ansi)
+               :features commander)
         
         (:name ido
                :description "Interactively Do Things. Fuzzy match buffers/files/etc."
                :type http
                :url "http://repo.or.cz/w/emacs.git/blob_plain/HEAD:/lisp/ido.el")
 
-        (:name impatient-mode
-               :description "See your html rendered in browser as yout type"
-               :type git
-               :url "https://github.com/netguy204/imp.el.git")
-        
-        (:name kanban
-               :description "Parse org-todo headlines to use org-tables as Kanban tables"
-               :type http
-               :url "https://bitbucket.org/ArneBab/kanban.el/raw/ce256090a5a4ef6db00a8ba78d9bd5838571d4ea/kanban.el"
-               :depends org-mode
-               :features kanban)
-        
         (:name kill-ring-search
                :description "Search the kill ring in the minibuffer."
                :type elpa)
-
-;; TODO troubleshoot this a bit
-        (:name org-annotation-quicksilver
-               :description "Allows creation of org notes from other applications via Quicksilver and remember-mode."
-               :type git
-               :url "https://github.com/jstautz/org-mac-protocol.git"
-               :depends org-mode)
-
+        
         (:name restclient
                :description "Manually explore and test HTTP REST webservices"
                :type git
@@ -112,36 +63,13 @@
                :compile ("json-reformat.el" "restclient.el")
                :features restclient)
 
-        (:name remember
-               :description "A mode for quickly jotting down things to remember"
-               :type git
-               :url "http://repo.or.cz/r/remember-el.git"
-               :compile ("remember.el")
-               :features remember)
-
-        (:name simple-httpd
-               :description "A simple Emacs webserver"
-               :type git
-               :url "https://github.com/skeeto/emacs-http-server.git")
-
-        (:name unbound
-               :description "Find convenient unbound keybindings."
-               :type http
-               :url "http://www.emacswiki.org/emacs/download/unbound.el"
-               :features unbound)
-
-        (:name wc-mode
-               :description "Running word count with goals (minor mode)"
-               :type git
-               :url "https://github.com/bnbeckwith/wc-mode.git"
-               :features wc-mode)
+        (:name smart-mode-line
+               :description "Sexy mode-line for Emacs"
+               :type github
+               :pkgname "Bruce-Connor/smart-mode-line"
+               :depends (dash)
+               :features smart-mode-line)
         
-        (:name wgrep
-               :description "Writable grep buffer, apply the changes to files."
-               :type git
-               :url "https://github.com/magnars/Emacs-wgrep.git"
-               :features wgrep)
-
         (:name writer-names
                :description "Generate random names (for fiction writers)"
                :type git
@@ -154,64 +82,33 @@
                          (defvar writer-last-names
                            (concat el-get-dir "/writer-names/census_data/dist.all.last")))
                :load "writer-names.el")
-               
-              ))
+))
 
-;; All packages to install
-;; TODO -- troubleshoot/test multi-term (yanking seemed weird) -- maybe problem with kill?
-(unwind-protect
-    (let (retval)
-      (condition-case ex
-          (setq retval (setq my-packages
-			     (append
-			      '(ace-jump-mode
-                    android-mode
-                    applescript-mode
-                    auto-complete
-                    auto-complete-css
-                    auto-complete-emacs-lisp
-                    auto-complete-yasnippet
-                    browse-kill-ring
-                    css-mode
-                    diminish
-                    edb
-                    elisp-slime-nav
-                    ;;ensime
-                    expand-region
-                    feature-mode
-                    git-modes
-                    ghc-mod
-                    haskell-mode
-                    ido-ubiquitous
-                    js2-mode
-                    magit
-                    markdown-mode
-                    multi-term
-                    multiple-cursors
-                    nxhtml
-                    org-mode
-                    psvn
-                    rainbow-mode
-                    scala-mode2
-                    smex
-                    smooth-scrolling
-                    undo-tree
-                    yaml-mode
-                    yasnippet)
-			      (mapcar 'el-get-source-name el-get-sources))))
-        ('error (message (format "Caught exception: [%s]" ex))))
-        retval)
-  (message "Attempting to build el-get package list..."))
+;; Packages to install from base el-get recipes
+(setq my-packages
+      (append
+       '(
+         ansi
+         browse-kill-ring
+         dash
+         diminish
+         expand-region
+         f
+         ido-ubiquitous
+         js2-mode
+         js2-refactor
+         magit
+         markdown-mode
+         multiple-cursors
+         org-mode
+         psvn
+         s
+         smex
+         yasnippet
+         )
+       (mapcar 'el-get-source-name el-get-sources)))
 
+(el-get 'sync my-packages)
 
-
-;; Sync packages. Wrapped in unwind-protect block in case el-get not yet installed
-(unwind-protect
-    (let (retval)
-      (condition-case ex
-          (setq retval (el-get 'sync my-packages))
-        ('error (message (format "Caught exception: [%s]" ex))))
-        retval)
-  (message "Attempting el-get sync..."))
- 
 (provide 'init-packages)
+
