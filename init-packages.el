f66923a79457e43974b4f0248736396be0864c9a
Jeff Stautz <jeff.stautz@gmail.com>
2014-04-05 23:18:28 -0700

    Updated gitignore and package handling

--- init-packages.el
+++ init-packages.el
@@ -5,28 +5,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;; Add a bunch of other repos to my sources list
-(require 'package)
-(dolist (source '( ("gnu" . "http://elpa.gnu.org/packages/")
-                   ("elpa" . "http://tromey.com/elpa/")
-                   ("marmalade" . "http://marmalade-repo.org/packages/")
-                   ("melpa" . "http://melpa.milkbox.net/packages/")))
-  (add-to-list 'package-archives source t))
-(package-initialize)
-(add-to-list 'load-path (concat dotemacs-dir "elpa"))
-(add-to-list 'load-path (concat dotemacs-dir "el-get/el-get"))
 
-;; el-get, install thyself
-(unless (require 'el-get nil t)
- (url-retrieve
-  "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-  (lambda (s)
-    (let ((el-get-master-branch) (el-get-install-skip-emacswiki-recipes))
-      (goto-char (point-max))
-      (eval-print-last-sexp)))))
-
-(setq el-get-dir (concat dotemacs-dir "el-get")
-      el-get-user-package-directory (concat dotemacs-dir "config"))
 
 
 ;; My custom el-get recipes 
