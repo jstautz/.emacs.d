f4c9126a23e3cd040c10ea97c6fcf1155d2b1c03
Jeff Stautz <jeff.stautz@gmail.com>
2014-04-07 23:00:59 -0700

    Use noweb references to tangle elisp
    Seems to work nicely. Not sure I'm explaining it well, though.

--- emacs-init.el
+++ emacs-init.el
@@ -1,19 +1,14 @@
 (require 'cl)
-
 (let ((jcs:shell-path (shell-command-to-string ". ~/.bashrc; echo -n $PATH")))
   (setenv "PATH" jcs:shell-path)
   (setq exec-path (split-string jcs:shell-path ":")))
-
 (setq home-dir "/Users/jstautz/"
       dotemacs-dir (file-name-directory (or load-file-name (buffer-file-name)))
       emacs-dir "/Applications/Emacs.app/Contents/"
       emacs-bin (concat emacs-dir "MacOS/Emacs")
       info-dir (concat emacs-dir "Resources/info/"))
-
 (setq custom-file (concat dotemacs-dir "init-customizations.el"))
-
 (add-to-list 'load-path dotemacs-dir)
-
 (defun jcs:decrypt-secrets ()
   (interactive)
   (require 'secrets))
@@ -25,10 +20,8 @@
                    ("melpa" . "http://melpa.milkbox.net/packages/")))
   (add-to-list 'package-archives source t))
 (package-initialize)
-
 (add-to-list 'load-path (concat dotemacs-dir "elpa"))
 (add-to-list 'load-path (concat dotemacs-dir "el-get/el-get"))
-
 ;; el-get, install thyself!
 (unless (require 'el-get nil t)
  (url-retrieve
@@ -37,16 +30,14 @@
     (let ((el-get-master-branch) (el-get-install-skip-emacswiki-recipes))
       (goto-char (point-max))
       (eval-print-last-sexp)))))
-
 (setq el-get-dir (concat dotemacs-dir "el-get")
       el-get-user-package-directory (concat dotemacs-dir "config"))
-
 (require 'init-packages)
 
 (require 'init-customizations)
 
+;; test
 (require 'init-custom-functions)
 
 (desktop-read)
-
 (server-start)
