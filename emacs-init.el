f70c7dfe6fdbbc3c1e3636343c3428f0db6d920c
Jeff Stautz <jeff.stautz@gmail.com>
2014-04-17 07:40:30 -0700

    Updated order of emacs-init.org. Minor changes.

--- emacs-init.el
+++ emacs-init.el
@@ -1,4 +1,57 @@
 (require 'cl)
+
+(let ((jcs:shell-path (shell-command-to-string ". ~/.bashrc; echo -n $PATH")))
+  (setenv "PATH" jcs:shell-path)
+  (setq exec-path (split-string jcs:shell-path ":")))
+
+(setq home-dir "/Users/jstautz/"
+      dotemacs-dir (file-name-directory (or load-file-name (buffer-file-name)))
+      emacs-dir "/Applications/Emacs.app/Contents/"
+      emacs-bin (concat emacs-dir "MacOS/Emacs")
+      info-dir (concat emacs-dir "Resources/info/"))
+
+(setq custom-file (concat dotemacs-dir "init-customizations.el"))
+
+(add-to-list 'load-path dotemacs-dir)
+
+(defun jcs:decrypt-secrets ()
+  (interactive)
+  (require 'secrets))
+
+(require 'package)
+(dolist (source '( ("gnu" . "http://elpa.gnu.org/packages/")
+                   ("elpa" . "http://tromey.com/elpa/")
+                   ("marmalade" . "http://marmalade-repo.org/packages/")
+                   ("melpa" . "http://melpa.milkbox.net/packages/")))
+  (add-to-list 'package-archives source t))
+(package-initialize)
+
+(add-to-list 'load-path (concat dotemacs-dir "elpa"))
+(add-to-list 'load-path (concat dotemacs-dir "el-get/el-get"))
+
+;; el-get, install thyself!
+(unless (require 'el-get nil t)
+ (url-retrieve
+  "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+  (lambda (s)
+    (let ((el-get-master-branch) (el-get-install-skip-emacswiki-recipes))
+      (goto-char (point-max))
+      (eval-print-last-sexp)))))
+
+(setq el-get-dir (concat dotemacs-dir "el-get")
+      el-get-user-package-directory (concat dotemacs-dir "config"))
+
+(require 'init-packages)
+
+(require 'init-customizations)
+
+(require 'init-custom-functions)
+
+(desktop-read)
+
+(server-start)
+
+(require 'cl)
 (let ((jcs:shell-path (shell-command-to-string ". ~/.bashrc; echo -n $PATH")))
   (setenv "PATH" jcs:shell-path)
   (setq exec-path (split-string jcs:shell-path ":")))
