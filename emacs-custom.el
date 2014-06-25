ff85d214c52edb5a57bf5b2f854dc53ecfb7544f
Jeff Stautz <jeff.stautz@gmail.com>
2014-06-25 09:41:57 -0700

    Minor updates

---
+++ emacs-custom.el
@@ -0,0 +1,12 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-loop-over-headlines-in-active-region (quote start-level)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
