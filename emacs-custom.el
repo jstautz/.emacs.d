0dec8e9592cd5843e21b3bdfa17b3b94a474cfbe
Jeff Stautz <jeff.stautz@gmail.com>
2014-10-10 22:19:48 -0700

    Added safe theme.
    But still has error messages?

--- emacs-custom.el
+++ emacs-custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-loop-over-headlines-in-active-region (quote start-level)))
+ '(custom-safe-themes
+   (quote
+    ("3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
