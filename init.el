d71331e902bd02044895d435e3b951e1b3bc830f
Jeff Stautz <jeff.stautz@gmail.com>
2017-09-17 21:49:19 -0700

    Major breaking change -- move to new structure (WIP)

--- init.el
+++ init.el
@@ -2,38 +2,47 @@
 ;;
 ;; Jeff's .emacs init file
 ;;
-;; - Sets up load paths, etc.
-;; - Defines my "decrypt-secrets" function for use in other files
-;; - Loads all packages (init-packages.el)
-;; - Loads custom keybindings, UI, and editing prefs (init-customizations.el)
-;; - Loads custom + util functions (init-custom-functions.el)
-;; - Fires up Emacs server
+;; - Set up custom package sources
+;; - Define directory variables
+;; - Set up my $PATH
+;; - Load separate files to set up packages, interface options, and org-mode
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(require 'cl)
 
-(setq exec-path (quote ("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/usr/local/bin"
-                        "/usr/X11/bin" "/opt/local/bin" "/usr/local/git/bin"
-                        "/Applications/Emacs.app/Contents/MacOS/bin"
-                        "/Users/jstautz/bin" "/usr/texbin")))
+;; Set up package sources
+(require 'package)
+(dolist (source '( ("gnu" . "http://elpa.gnu.org/packages/")
+                   ("elpa" . "http://tromey.com/elpa/")
+		   ("org"         . "http://orgmode.org/elpa/")
+                   ("melpa" . "http://melpa.milkbox.net/packages/")))
+  (add-to-list 'package-archives source t))
+(package-initialize)
 
-(setq home-dir "/Users/jstautz/"
-      dotemacs-dir (file-name-directory (or load-file-name (buffer-file-name)))
+;; Assume current directory is the dot-emacs directory
+(setq dotemacs-dir (file-name-directory (or load-file-name (buffer-file-name))))
+(add-to-list 'load-path (concat dotemacs-dir "lisp/"))
+
+;; Set some other useful environment vars, specific to my setup
+(setq home-dir "/Users/jeff.stautz/"
       emacs-dir "/Applications/Emacs.app/Contents/"
-      custom-file (concat dotemacs-dir "init-customizations.el")
       emacs-bin (concat emacs-dir "MacOS/Emacs")
       info-dir (concat emacs-dir "Resources/info/"))
 
-(add-to-list 'load-path dotemacs-dir)
+;; Gross hack to make my $PATH environment var the same as in bash
+(let ((jcs:shell-path (shell-command-to-string ". ~/.bashrc; echo -n $PATH")))
+  (setenv "PATH" jcs:shell-path)
+  (setq exec-path (split-string jcs:shell-path ":")))
 
-;; Decrypt and load secrets.el.gpg file containing passwords, etc.
-(defun jcs:decrypt-secrets ()
-  (interactive)
-  (require 'secrets))
 
-(require 'init-packages)
-(require 'init-customizations)
-(require 'init-custom-functions)
+;; For the sake of this demo, I'm splitting this into separate .el files
 
-(server-start)
+;; Bootstrap & install packages:
+(load-file (concat dotemacs-dir "lisp/packages.el"))
+
+;; Set up interface and editor options the way I like 'em:
+(load-file (concat dotemacs-dir "lisp/settings.el"))
+
+;; And now, load org-mode and its settings:
+;;(load-file (concat dotemacs-dir "lisp/org-mode.el"))
+(load-file "~/Desktop/emacs.d/config/init-org-mode.el")
