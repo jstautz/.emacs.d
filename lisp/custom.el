1b3369bff6d7ef85d50d7c36201816e28aae940a
Jeff Stautz <jeff.stautz@gmail.com>
2020-05-16 14:22:23 -0700

    Add htmlize

--- lisp/custom.el
+++ lisp/custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   (quote
+    (htmlize web-mode web-beautify use-package smex restclient php-mode org-pomodoro org-plus-contrib markdown-mode magit ido-vertical-mode ido-completing-read+ guide-key browse-kill-ring ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
