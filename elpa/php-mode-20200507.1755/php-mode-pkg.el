47c608f43a380db34e6b73367df19aafc75409e4
Jeff Stautz <jeff.stautz@gmail.com>
2020-06-05 00:16:43 -0700

    Update a whole buncha packages

---
+++ elpa/php-mode-20200507.1755/php-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "php-mode" "20200507.1755" "Major mode for editing PHP code"
+  '((emacs "24.3"))
+  :commit "a7687e454adc2e6c1e13d90beac122af32169eb8" :keywords
+  '("languages" "php")
+  :authors
+  '(("Eric James Michael Ritz"))
+  :maintainer
+  '("USAMI Kenta" . "tadsan@zonu.me")
+  :url "https://github.com/emacs-php/php-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
