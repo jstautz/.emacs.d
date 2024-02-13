69458e7de148d4090b0875078209b9523b70d042
Jeff Stautz <jeff.stautz@gmail.com>
2024-02-13 09:27:10 -0800

    Add packages: updated org-contrib, html-to-markdown

---
+++ elpa/html-to-markdown-20151105.840/html-to-markdown-pkg.el
@@ -0,0 +1,14 @@
+(define-package "html-to-markdown" "20151105.840" "HTML to Markdown converter written in Emacs-lisp."
+  '((cl-lib "0.5"))
+  :commit "60c5498c801be186478cf7c05be05b4430c4a144" :authors
+  '(("Artur Malabarba" . "bruce.connor.am@gmail.com"))
+  :maintainers
+  '(("Artur Malabarba" . "bruce.connor.am@gmail.com"))
+  :maintainer
+  '("Artur Malabarba" . "bruce.connor.am@gmail.com")
+  :keywords
+  '("tools" "wp" "languages")
+  :url "http://github.com/Bruce-Connor/html-to-markdown")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
