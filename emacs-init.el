2e5982d5bda6dcbd4af9789c9034528306462713
Jeff Stautz <jeff.stautz@gmail.com>
2024-04-23 00:21:21 -0700

    Add obsidian-mode config

--- emacs-init.el
+++ emacs-init.el
@@ -547,3 +547,33 @@ of windows in the frame simply by calling this command again."
 (setq diff-switches "-a -c")
 
 (org-babel-load-file (concat user-emacs-directory "org-mode-init.org"))
+
+(use-package obsidian
+  :ensure t
+  :demand t
+  :config
+  (obsidian-specify-path "/Users/jstautz/Library/Mobile Documents/iCloud~md~obsidian/Documents/Jeff's Vault")
+  (global-obsidian-mode t)
+  :custom
+  ;; This directory will be used for `obsidian-capture' if set.
+  (obsidian-inbox-directory "_Inbox")
+  ;; Create missing files in inbox? - when clicking on a wiki link
+  ;; t: in inbox, nil: next to the file with the link
+  ;; default: t
+  ;(obsidian-wiki-link-create-file-in-inbox nil)
+  ;; The directory for daily notes (file name is YYYY-MM-DD.md)
+  (obsidian-daily-notes-directory "_Daily")
+  ;; Directory of note templates, unset (nil) by default
+  (obsidian-templates-directory "zz_Templates")
+  ;; Daily Note template name - requires a template directory. Default: Daily Note Template.md
+  ;; (setq obsidian-daily-note-template "Daily Note Template.md")
+  :bind (:map obsidian-mode-map
+  ;; Replace C-c C-o with Obsidian.el's implementation. It's ok to use another key binding.
+  ("C-c C-o" . obsidian-follow-link-at-point)
+  ;; Jump to backlinks
+  ("C-c C-b" . obsidian-backlink-jump)
+  ;; If you prefer you can use `obsidian-insert-link'
+  ("C-c C-l" . obsidian-insert-wikilink)))
+
+;; Ready to rock!
+(desktop-read)
