50510e018a4b1ead105e4f2860e639896d2c5204
Jeff Stautz <jeff.stautz@gmail.com>
2024-10-20 23:51:01 -0700

    Update package list

--- lisp/custom.el
+++ lisp/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(warning-suppress-types '((org-element-cache))))
+ '(package-selected-packages
+   '(org-appear web-mode web-beautify visual-fill-column use-package smex restclient php-mode org-pomodoro org-contrib obsidian magit-popup magit ido-vertical-mode ido-completing-read+ htmlize html-to-markdown guide-key diminish browse-kill-ring ace-jump-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
